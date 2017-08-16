554737e8422c3294018c49424117d9a5221b524b
Mounika <mounika.tunga9@gmail.com>
2017-08-16 10:48:32 -0700

    deleting a record

--- Controllers/WeddingController.cs
+++ Controllers/WeddingController.cs
@@ -12,15 +12,16 @@ namespace wedding_planner.Controllers
     public class WeddingController : Controller
     {
         private WeddingContext _context;
- 
+
         public WeddingController(WeddingContext context)
         {
             _context = context;
         }
         [HttpGet]
         [Route("show")]
-        public IActionResult show(){
-        
+        public IActionResult show()
+        {
+
             List<Wedding> wedd = _context.weddings
                                         .Include(wedding => wedding.Invitations)
                                         .ToList();
@@ -40,35 +41,68 @@ namespace wedding_planner.Controllers
         public IActionResult Wedform(Wedding NewWedding)
         {
             System.Console.WriteLine("adding newwedding");
-            
-             var loggedUserId = HttpContext.Session.GetInt32("UserId");
 
-            
-                NewWedding.UserId = loggedUserId.Value;
-            
-                _context.Add(NewWedding);
-                _context.SaveChanges();
-             
-                return RedirectToAction("show");
-            
+            var loggedUserId = HttpContext.Session.GetInt32("UserId");
+
+
+            NewWedding.UserId = loggedUserId.Value;
+
+            _context.Add(NewWedding);
+            _context.SaveChanges();
+
+            return RedirectToAction("show");
+
         }
         [HttpGet]
         [Route("wedding/deletewed/{weddingid}")]
         public IActionResult deletewed(int weddingid)
         {
-                Wedding weddingrecord = _context.weddings.SingleOrDefault(w => w.WeddingId == weddingid);
-                _context.weddings.Remove(weddingrecord);
-                _context.SaveChanges();
-                return RedirectToAction("show"); 
+            Wedding weddingrecord = _context.weddings.SingleOrDefault(w => w.WeddingId == weddingid);
+            _context.weddings.Remove(weddingrecord);
+            _context.SaveChanges();
+            return RedirectToAction("show");
         }
 
         [HttpGet]
-        [Route("wedding/{WeddingId}")]
+        [Route("wedding/attending/{weddingid}")]
+        public IActionResult attending(int weddingid)
         {
-            public IActionResult details(int WeddingId){
+            Invitation newinv = new Invitation();
+            int userid = (int)HttpContext.Session.GetInt32("UserId");
+            newinv.UserId = userid;
+            newinv.WeddingId = weddingid;
 
-            }
+            _context.Add(newinv);
+            _context.SaveChanges();
+            return RedirectToAction("show");
         }
+        [HttpGet]
+        [Route("wedding/notattending/{weddingid}")]
+        public IActionResult notattending(int weddingid)
+        {
+            Invitation newinv = new Invitation();
+            int userid = (int)HttpContext.Session.GetInt32("uid");
+            Invitation invitationrecord = _context.invitations.SingleOrDefault(w => w.WeddingId == weddingid && w.UserId == userid);
+            _context.invitations.Remove(invitationrecord);
+            _context.SaveChanges();
+
+            return RedirectToAction("show");
+        }
+        [HttpGet]
+        [Route("wedding/details/{weddingid}")]
+        public IActionResult show(int weddingid)
+        {
+            int userid = (int)HttpContext.Session.GetInt32("UserId");
+            Wedding weddingrecord = _context.weddings.SingleOrDefault(w => w.WeddingId == weddingid);
+            List<Invitation> invitationrecords = _context.invitations.Where(w => w.WeddingId == weddingid)
+            .Include(p => p.user)
+            .ToList();
+
+            ViewBag.invitationrecords = invitationrecords;
+            ViewBag.weddingrecord = weddingrecord;
+            return View("details");
+        }
+
     }
 }
-        
+
